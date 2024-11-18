4452674cf852d35c382e222dfc24b82d62355e67
Joy-mwende63 <mwendejoy47@gmail.com>
2024-11-18 23:13:16 +0300

    react-hooks-lists-and-keys-lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,60 @@
+// // import React from "react";
+// // import ProjectItem from "./ProjectItem";
+
+// // function ProjectList({ projects }) {
+// //   console.log(projects);
+// //   return (
+// //     <div id="projects">
+// //       <h2>My Projects</h2>
+// //       <div id="project-list">{/* render ProjectItem components here */}</div>
+// //     </div>
+// //   );
+// // }
+
+// // export default ProjectList;
+
+// import React from "react";
+// import ProjectItem from "./ProjectItem";
+
+// function ProjectList({ projects }) {
+//   console.log(projects); // This will log the projects array
+
+//   return (
+//     <div id="projects">
+//       <h2>My Projects</h2>
+//       <div id="project-list">
+//         {projects.map((project) => (
+//           <ProjectItem
+//             key={project.id} // Use the id as the unique key
+//             title={project.title}
+//             description={project.description}
+//             technologies={project.technologies}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProjectList;
+
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id} // Ensure the key is unique
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
